526fa49ff4ac7069a96b4d5398bbdaf026fe3b85
Nafis Ahmed <nafisahmed@iut-dhaka.edu>
2024-02-13 01:52:58 +0600

    redux added for state management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,6 +12,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import HomeScreen from './pages/home-screen.tsx'
 import ProductScreen from './pages/product-screen.tsx'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,6 +26,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
